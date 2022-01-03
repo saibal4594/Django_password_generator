3641751343d5339e8687ea00a722e5083cc8df85
Saibal <mr.chakrabortysaibal94@gmail.com>
2022-01-03 17:59:13 +0530

    first commit

---
+++ dummy_first/pwd_gen/gen_pwd/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GenPwdConfig(AppConfig):
+    name = 'gen_pwd'
---
+++ dummy_first/pwd_gen/gen_pwd/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'gen_pwd/home.html')
+def about(request):
+    return render(request, 'gen_pwd/about.html')
+
+def password(request):
+    character=list("abcdedfghijklmnopqrstuvwxyz")
+    
+    thepassword=" "
+    length=int(request.GET.get("length",12))
+    if request.GET.get("uppercase"):
+        character.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    if request.GET.get("number"):
+        character.extend(list("0123456789"))
+    if request.GET.get("special character"):
+        character.extend(list("!@#$%^&*"))
+    for i in range(length):
+        thepassword+=random.choice(character)
+    return render(request, 'gen_pwd/password.html',{"password": thepassword})
